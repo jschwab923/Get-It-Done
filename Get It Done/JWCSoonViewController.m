1c0367780111fcd726ba4ffbd6460380bbd40672
Jeff Schwab <jschwab923@gmail.com>
2014-02-01 17:41:56 -0800

    Views correctly repositioning/sizing based on device orientation and screen size

--- Get It Done/JWCSoonViewController.m
+++ Get It Done/JWCSoonViewController.m
@@ -9,12 +9,14 @@
 #import "JWCSoonViewController.h"
 #import "M13ProgressViewPie.h"
 #import "UIColor+GetItDoneColors.h"
+#import "JWCTaskManager.h"
 
 @interface JWCSoonViewController ()
 
 {
     NSLayoutConstraint *_landScapeCollectionViewTopConstraint;
     NSLayoutConstraint *_landScapeCollectionViewLeftConstraint;
+    NSLayoutConstraint *_landScapeProgressViewConstraint;
     NSLayoutConstraint *_portraitCollectionViewBottomConstraint;
 }
 
@@ -41,8 +43,6 @@
 {
     [super viewDidLoad];
     
-    [self setUpConstraints];
-    
     self.label.textColor = [UIColor darkBlueColor];
     
     self.progressViewPie = [[M13ProgressViewPie alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
@@ -51,9 +51,10 @@
     self.progressViewPie.primaryColor = [UIColor darkBlueColor];
     self.progressViewPie.secondaryColor = [UIColor darkBlueColor];
     
-    //TODO: Remove this default progress and base on current focus duration
+    //TODO: Remove this default progress and base on current progress
     [self.progressViewPie setProgress:.7 animated:YES];
     
+    [self setUpConstraintsAndFramesForCurrentDevice];
     
 }
 
@@ -66,38 +67,52 @@
 #pragma mark - Rotation Handling
 - (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
 {
-    
+    [self updateCustomConstraints];
+}
+
+#pragma mark - Contraints and Orientation Methods
+
+- (void)updateCustomConstraints
+{
     if ([[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeRight ||
         [[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeLeft)
     {
         [self.view removeConstraint:_portraitCollectionViewBottomConstraint];
         [self.view addConstraints:@[_landScapeCollectionViewTopConstraint,
-                                    _landScapeCollectionViewLeftConstraint]];
+                                    _landScapeCollectionViewLeftConstraint,
+                                    _landScapeProgressViewConstraint]];
     } else {
         [self.view removeConstraints:@[_landScapeCollectionViewTopConstraint,
-                                       _landScapeCollectionViewLeftConstraint]];
+                                       _landScapeCollectionViewLeftConstraint,
+                                       _landScapeProgressViewConstraint]];
         
         [self.view addConstraint:_portraitCollectionViewBottomConstraint];
     }
-    
 }
 
-#pragma mark - Convenience Methods
-
-- (void)setUpConstraints
+- (void)setUpConstraintsAndFramesForCurrentDevice
 {
     // Setup contraints for handling rotation
     _landScapeCollectionViewTopConstraint = [NSLayoutConstraint constraintWithItem:self.collectionViewTasks attribute:NSLayoutAttributeTop relatedBy:0 toItem:self.progressContainerView attribute:NSLayoutAttributeTop multiplier:1 constant:0];
-    _landScapeCollectionViewLeftConstraint = [NSLayoutConstraint constraintWithItem:self.collectionViewTasks attribute:NSLayoutAttributeTrailing relatedBy:0 toItem:self.progressContainerView attribute:NSLayoutAttributeLeading multiplier:1 constant:5];
+    _landScapeCollectionViewLeftConstraint = [NSLayoutConstraint constraintWithItem:self.collectionViewTasks attribute:NSLayoutAttributeLeft relatedBy:0 toItem:self.progressContainerView attribute:NSLayoutAttributeRight multiplier:1 constant:5];
+    
+    _landScapeProgressViewConstraint = [NSLayoutConstraint constraintWithItem:self.progressContainerView attribute:NSLayoutAttributeLeft relatedBy:0 toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1 constant:5];
     
     _portraitCollectionViewBottomConstraint = [NSLayoutConstraint constraintWithItem:self.collectionViewTasks attribute:NSLayoutAttributeBottom relatedBy:0 toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
     
-    // Add appropriate constraints based on initial device orientation
+    // Add appropriate constraints based on initial device orientation and screen size
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+
     if ([[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeRight ||
-        [[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeRight)
+        [[UIDevice currentDevice] orientation] == UIDeviceOrientationLandscapeLeft)
     {
-        [self.view addConstraints:@[_landScapeCollectionViewTopConstraint,
-                                    _landScapeCollectionViewLeftConstraint]];
+        if (screenSize.height == 480) {
+            [self.view addConstraints:@[_landScapeCollectionViewTopConstraint,
+                                        _landScapeProgressViewConstraint]];
+        } else if (screenSize.height == 568) {
+            [self.view addConstraints:@[_landScapeCollectionViewTopConstraint,
+                                        _landScapeProgressViewConstraint]];
+        }
     } else {
         [self.view addConstraint:_portraitCollectionViewBottomConstraint];
     }
